3a2c402011aaa06a045f87fe14dcebc058d127ea
Benno Zeeman <bzeeman@live.nl>
2021-04-06 15:48:22 +0200

    fix: give nodes fixed port

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ sn_node --skip-igd --first=$ip:12000 --root-dir "${node_dir}/12000" &
 # Regular nodes connecting to genesis.
 for port in $(seq 12001 12011); do
     sleep 1
-    sn_node --skip-igd --local-addr=$ip:$port --hard-coded-contacts="[\"$ip:12000\"]" --root-dir "${node_dir}/$port" &
+    sn_node --skip-igd --local-addr=$ip:$port --public-addr=$ip:$port --hard-coded-contacts="[\"$ip:12000\"]" --root-dir "${node_dir}/$port" &
 done
 
 # Wait for initialisation.
