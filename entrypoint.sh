e64e4737217483a78598d7acfb3319e03d55efc7
Benno Zeeman <bzeeman@live.nl>
2021-04-07 22:27:12 +0200

    feat: bind to all addresses

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -eax
 
-ip=$(hostname -i | awk '{print $1}')
+ip="${SN_PUBLIC_IP:-$(hostname -i | awk '{print $1}')}"
 
 node_dir="${HOME}/.safe/node"
 mkdir --parents "${node_dir}"
 
 # Genesis node.
-sn_node --skip-igd --first=$ip:12000 --root-dir "${node_dir}/12000" &
+sn_node --skip-igd --first=0.0.0.0:12000 --root-dir "${node_dir}/12000" &
 
 # Regular nodes connecting to genesis.
 for port in $(seq 12001 12011); do
     sleep 1
-    sn_node --skip-igd --local-addr=$ip:$port --public-addr=$ip:$port --hard-coded-contacts="[\"$ip:12000\"]" --root-dir "${node_dir}/$port" &
+    sn_node --skip-igd --local-addr=0.0.0.0:$port --public-addr=$ip:$port --hard-coded-contacts="[\"127.0.0.1:12000\"]" --root-dir "${node_dir}/$port" &
 done
 
 # Wait for initialisation.
